f8e05a9a3aa1174c1622533c44f5d3c471a6d31a
karamvirsingh98 <67458484+karamvirsingh98@users.noreply.github.com>
2022-06-02 17:16:40 -0700

    overview layout and styles

--- src/screens/block_details/components/overview/index.tsx
+++ src/screens/block_details/components/overview/index.tsx
@@ -7,14 +7,67 @@ import { useRecoilValue } from 'recoil';
 import { useProfileRecoil } from '@recoil/profiles';
 import { readDate } from '@recoil/settings';
 import {
-  BoxDetails, AvatarName,
+  BoxDetails, AvatarName, Box,
 } from '@components';
 import { OverviewType } from '../../types';
+import { useStyles } from './styles';
 
 const Overview: React.FC<OverviewType & ComponentDefault> = (props, { className }) => {
   const proposer = useProfileRecoil(props.proposer);
   const { t } = useTranslation('blocks');
   const dateFormat = useRecoilValue(readDate);
+
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.root}>
+      <Typography variant="h2">
+        {t('overview')}
+      </Typography>
+      <div className={classes.item}>
+        <div className="label">
+          {t('hash')}
+        </div>
+        {props.hash}
+      </div>
+      <div className={classes.details}>
+        <div className={classes.item}>
+          <div className="label">
+            {t('height')}
+          </div>
+          <div>
+            {props.height}
+          </div>
+        </div>
+        <div className={classes.item}>
+          <div className="label">
+            {t('time')}
+          </div>
+          <div>
+            {formatDayJs(dayjs.utc(props.timestamp), dateFormat)}
+          </div>
+        </div>
+        <div className={classes.item}>
+          <div className="label">
+            {t('proposer')}
+          </div>
+          <AvatarName
+            address={props.proposer}
+            imageUrl={proposer.imageUrl}
+            name={proposer.name}
+          />
+        </div>
+        <div className={classes.item}>
+          <div className="label">
+            {t('txs')}
+          </div>
+          <div>
+            {numeral(props.txs).format('0,0')}
+          </div>
+        </div>
+      </div>
+    </Box>
+  );
 
   return (
     <BoxDetails
