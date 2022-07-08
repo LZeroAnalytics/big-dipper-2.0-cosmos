778383840185c24f33c33007bd19fd047c4b9e8b
karamvirsingh98 <67458484+karamvirsingh98@users.noreply.github.com>
2022-07-08 10:28:38 -0700

    title for params page

--- src/screens/params/index.tsx
+++ src/screens/params/index.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import useTranslation from 'next-translate/useTranslation';
 import { NextSeo } from 'next-seo';
 import {
-  Layout,
-  LoadAndExist,
-  BoxDetails,
+  Layout, LoadAndExist, BoxDetails,
 } from '@components';
+import { Typography } from '@material-ui/core';
 import { useStyles } from './styles';
 import { useParams } from './hooks';
 import {
@@ -21,30 +20,40 @@ const Params = () => {
   const classes = useStyles();
   const { state } = useParams();
 
-  const staking = state.staking ? {
-    title: t('staking'),
-    details: formatStaking(state.staking, t),
-  } : null;
+  const staking = state.staking
+    ? {
+      title: t('staking'),
+      details: formatStaking(state.staking, t),
+    }
+    : null;
 
-  const slashing = state.slashing ? {
-    title: t('slashing'),
-    details: formatSlashing(state.slashing, t),
-  } : null;
+  const slashing = state.slashing
+    ? {
+      title: t('slashing'),
+      details: formatSlashing(state.slashing, t),
+    }
+    : null;
 
-  const minting = state.minting ? {
-    title: t('minting'),
-    details: formatMinting(state.minting, t),
-  } : null;
+  const minting = state.minting
+    ? {
+      title: t('minting'),
+      details: formatMinting(state.minting, t),
+    }
+    : null;
 
-  const distribution = state.distribution ? {
-    title: t('distribution'),
-    details: formatDistribution(state.distribution, t),
-  } : null;
+  const distribution = state.distribution
+    ? {
+      title: t('distribution'),
+      details: formatDistribution(state.distribution, t),
+    }
+    : null;
 
-  const gov = state.gov ? {
-    title: t('gov'),
-    details: formatGov(state.gov, t),
-  } : null;
+  const gov = state.gov
+    ? {
+      title: t('gov'),
+      details: formatGov(state.gov, t),
+    }
+    : null;
 
   return (
     <>
@@ -54,31 +63,18 @@ const Params = () => {
           title: t('params'),
         }}
       />
-      <Layout
-        navTitle={t('params')}
-      >
-        <LoadAndExist
-          loading={state.loading}
-          exists={state.exists}
-        >
+      <Layout navTitle={t('params')}>
+        <LoadAndExist loading={state.loading} exists={state.exists}>
+          <Typography variant="h1" className={classes.header}>
+            {t('params')}
+          </Typography>
+
           <span className={classes.root}>
-            {staking && (
-            <BoxDetails {...staking} />
-            )}
-            {
-            slashing && (
-              <BoxDetails {...slashing} />
-            )
-          }
-            {minting && (
-            <BoxDetails {...minting} />
-            )}
-            {distribution && (
-            <BoxDetails {...distribution} />
-            )}
-            {gov && (
-            <BoxDetails {...gov} />
-            )}
+            {staking && <BoxDetails {...staking} />}
+            {slashing && <BoxDetails {...slashing} />}
+            {minting && <BoxDetails {...minting} />}
+            {distribution && <BoxDetails {...distribution} />}
+            {gov && <BoxDetails {...gov} />}
           </span>
         </LoadAndExist>
       </Layout>
