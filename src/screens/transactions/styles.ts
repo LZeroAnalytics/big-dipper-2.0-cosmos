1428a986f4d9d44d877704fef3038b98cbf64e0c
karamvirsingh98 <67458484+karamvirsingh98@users.noreply.github.com>
2022-10-06 10:35:43 -0700

    fix scroll issues for transactions list

--- src/screens/transactions/styles.ts
+++ src/screens/transactions/styles.ts
@@ -13,7 +13,7 @@ export const useStyles = () => {
         box: {
           minHeight: '500px',
           height: '50vh',
-          padding: '0px !important',
+          padding: `0px 0px ${theme.spacing(4)}px 0px !important`,
           [theme.breakpoints.up('lg')]: {
             height: '100%',
             minHeight: '65vh',
