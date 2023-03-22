5e131894a05ab471b9bb79c8a9d4e1495da7669d
Juan Camilo Cifuentes <jccifuentes21@gmail.com>
2023-03-22 13:01:12 -0700

    added tooltips on hover on sidebar

--- packages/ui/src/components/nav/components/menu_items/index.tsx
+++ packages/ui/src/components/nav/components/menu_items/index.tsx
@@ -3,6 +3,7 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import useAppTranslation from '@/hooks/useAppTranslation';
+import { Tooltip } from '@mui/material';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { getMenuItems } from '@/components/nav/components/menu_items/utils';
@@ -39,22 +40,32 @@ const MenuItems = () => {
         }
 
         return (
-          <Link shallow key={x.key} href={x.url} legacyBehavior passHref>
-            <ListItemButton
-              className={cx(classes.root, {
-                active: isActive,
-              })}
-              component="a"
-            >
-              <ListItemIcon className={cx(classes.listItemIcon, isActive ? 'active' : '')}>
-                {isActive ? x.iconActive : x.iconInactive}
-              </ListItemIcon>
-              <ListItemText className={classes.listItemText} primary={t(x.key)} />
-            </ListItemButton>
-          </Link>
+          <Tooltip key={x.key} title={t(x.key)} placement="right" arrow enterDelay={650}>
+            <div>
+              <Link shallow key={`link-${x.key}`} href={x.url} legacyBehavior passHref>
+                <ListItemButton
+                  className={cx(classes.root, {
+                    active: isActive,
+                  })}
+                  component="a"
+                >
+                  <ListItemIcon className={cx(classes.listItemIcon, isActive ? 'active' : '')}>
+                    {isActive ? x.iconActive : x.iconInactive}
+                  </ListItemIcon>
+                  <ListItemText className={classes.listItemText} primary={t(x.key)} />
+                </ListItemButton>
+              </Link>
+            </div>
+          </Tooltip>
         );
       })}
-      {windowWidth > 1024 && <SettingsList className={classes.root} />}
+      {windowWidth > 1024 && (
+        <Tooltip title={t('settings')} placement="right" arrow enterDelay={800}>
+          <div>
+            <SettingsList className={classes.root} />
+          </div>
+        </Tooltip>
+      )}
       {windowWidth < 1025 && <NetworkSelector />}
     </List>
   );
