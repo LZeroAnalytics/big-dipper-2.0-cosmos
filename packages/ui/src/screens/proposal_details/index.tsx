207732a58ddb5f81e97fade474968d5fd4ec3fd0
Oleksandr Khlopiachyi <akhlopiachyi@gmail.com>
2024-01-18 12:46:50 +0200

    feat: add breadcrumb for proposal details page

--- packages/ui/src/screens/proposal_details/index.tsx
+++ packages/ui/src/screens/proposal_details/index.tsx
@@ -9,6 +9,7 @@ import VotesGraph from '@/screens/proposal_details/components/votes_graph';
 import { useProposalDetails } from '@/screens/proposal_details/hooks';
 import useStyles from '@/screens/proposal_details/styles';
 import { shouldShowData } from '@/screens/proposal_details/utils';
+import Link from 'next/link';
 
 const ProposalDetails = () => {
   const { t } = useTranslation('proposals');
@@ -27,6 +28,21 @@ const ProposalDetails = () => {
       <Layout navTitle={t('proposalDetails') ?? undefined}>
         <LoadAndExist exists={state.exists} loading={state.loading}>
           <span className={classes.root}>
+            <div className={classes.block}>
+              <Link href="/proposals" className={classes.breadcrumb}>
+                <svg
+                  width="16"
+                  height="17"
+                  viewBox="0 0 16 17"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M10.5 13.5L5.5 8.5L10.5 3.5" stroke="#25D695" strokeWidth="1.5" />
+                </svg>
+                Back to all proposals
+              </Link>
+              <div className={classes.title}>{t('proposalDetails')}</div>
+            </div>
             <Overview className={classes.overview} overview={overview} />
             {shouldShowData(overview.status) && <VotesGraph className={classes.votesGraph} />}
             {shouldShowData(overview.status) && <Votes className={classes.votes} />}
