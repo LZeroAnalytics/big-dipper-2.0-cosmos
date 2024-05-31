a0eabe77a6815cd9d44123731e102cc14ce02cbf
Oleksandr Khlopiachyi <akhlopiachyi@gmail.com>
2024-05-31 12:32:28 +0300

    fix: update msg to ibc_transfer

--- packages/ui/src/components/msg/ibc_transfer/transfer/index.tsx
+++ packages/ui/src/components/msg/ibc_transfer/transfer/index.tsx
@@ -4,34 +4,61 @@ import { FC } from 'react';
 import Name from '@/components/name';
 import { MsgTransfer } from '@/models';
 import { useProfileRecoil } from '@/recoil/profiles/hooks';
-import { formatNumber, formatToken } from '@/utils/format_token';
-import { Asset } from '@/screens/assets/hooks';
+import { formatToken } from '@/utils/format_token';
+import { Asset, convertHexToString } from '@/screens/assets/hooks';
+import { formatNumberWithThousandsSeparator } from '@/screens/account_details/components/other_tokens/components/desktop';
+import Big from 'big.js';
 
-const Transfer: FC<{ message: MsgTransfer; assets: Asset[] }> = (props) => {
-  const { message, assets } = props;
+const Transfer: FC<{ message: MsgTransfer; assets: Asset[]; metadatas: any[] }> = (props) => {
+  const { message, assets, metadatas } = props;
 
   const sender = useProfileRecoil(message.sender);
   const senderMoniker = sender ? sender?.name : message.sender;
   const receiver = useProfileRecoil(message.receiver);
   const receiverMoniker = receiver ? receiver?.name : message.receiver;
   const tokenFormatDenom = formatToken(message.token?.amount, message.token?.denom);
-  let token = `${formatNumber(
-    tokenFormatDenom.value,
-    tokenFormatDenom.exponent
-    // Kept the "toUpperCase()" in order to show the token symbol in uppercase
-  )} ${tokenFormatDenom.displayDenom.toUpperCase()}`;
+
+  const asset = metadatas.find(
+    (item) => item.base.toLowerCase() === tokenFormatDenom.displayDenom.toLowerCase()
+  );
   const tokenInAssets = assets.find(
     (assetItem) => tokenFormatDenom.displayDenom.toLowerCase() === assetItem.denom.toLowerCase()
   );
+  let amount = formatToken(message.token?.amount, message.token?.denom).value;
+  let displayDenom = asset
+    ? asset.display.toUpperCase()
+    : tokenFormatDenom.displayDenom.toUpperCase();
+
+  if (asset?.denom_units[1].exponent) {
+    const availableValue = new Big(+(message.token?.amount || '0'))
+      .div(Big(10).pow(asset?.denom_units[1].exponent))
+      .toFixed(asset?.denom_units[1].exponent);
+
+    amount = formatNumberWithThousandsSeparator(availableValue);
+  }
+
+  if (
+    tokenInAssets &&
+    tokenInAssets?.extra.xrpl_info &&
+    tokenInAssets?.extra.xrpl_info.source_chain === 'XRPL'
+  ) {
+    displayDenom =
+      tokenInAssets?.extra.xrpl_info.currency.length === 40
+        ? convertHexToString(tokenInAssets?.extra.xrpl_info.currency)
+        : tokenInAssets?.extra.xrpl_info.currency;
+  }
+
+  let parsedAmount = `${amount} ${displayDenom}`;
 
   if (tokenInAssets) {
     if (tokenFormatDenom.displayDenom.includes('ibc')) {
       const tokenDenom = tokenInAssets.extra.ibc_info!.display_name;
-      token = `${formatNumber(
-        String(+tokenFormatDenom.value / 10 ** tokenInAssets.extra.ibc_info!.precision),
-        tokenInAssets.extra.ibc_info!.precision
-        // Kept the "toUpperCase()" in order to show the token symbol in uppercase
-      )} ${tokenDenom}`;
+      const availableValue = new Big(+(message.token?.amount || '0'))
+        .div(Big(10).pow(tokenInAssets.extra.ibc_info!.precision))
+        .toFixed(tokenInAssets.extra.ibc_info!.precision);
+      amount = formatNumberWithThousandsSeparator(availableValue);
+
+      parsedAmount = `${amount} ${tokenDenom}`;
     }
   }
 
@@ -45,7 +72,7 @@ const Transfer: FC<{ message: MsgTransfer; assets: Asset[] }> = (props) => {
           <b />,
         ]}
         values={{
-          token,
+          token: parsedAmount,
           sourceChannel: message.sourceChannel,
         }}
       />
