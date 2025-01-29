873edee4d8d30fdcd151e730c18bbf1f07c5706e
Oleksandr Khlopiachyi <akhlopiachyi@gmail.com>
2025-01-29 12:48:38 +0200

    fix: update template for dex place order to highlight the most important information

--- packages/ui/src/components/msg/dex/place_order/index.tsx
+++ packages/ui/src/components/msg/dex/place_order/index.tsx
@@ -5,10 +5,12 @@ import { Trans } from 'react-i18next';
 import Name from '@/components/name';
 import { MsgPlaceOrder } from '@/models';
 import Big from 'big.js';
+import useStyles from './styles';
 
 const PlaceOrder: FC<{ message: MsgPlaceOrder }> = (props) => {
   const { message } = props;
   const sender = useProfileRecoil(message.sender);
+  const { classes } = useStyles();
 
   const price = useMemo(() => {
     try {
@@ -24,7 +26,11 @@ const PlaceOrder: FC<{ message: MsgPlaceOrder }> = (props) => {
     <Typography>
       <Trans
         i18nKey="message_contents:msgPlaceOrder"
-        components={[<Name address={message.sender} name={sender.name ?? message.sender} />, <b />]}
+        components={[
+          <Name address={message.sender} name={sender.name ?? message.sender} />,
+          <b />,
+          <span className={classes.root} />,
+        ]}
         values={{
           sender: message.sender,
           id: message.id,
---
+++ packages/ui/src/components/msg/dex/place_order/styles.ts
@@ -0,0 +1,9 @@
+import { makeStyles } from 'tss-react/mui';
+
+const useStyles = makeStyles()((theme) => ({
+  root: {
+    fontSize: theme.spacing(1.75),
+  },
+}));
+
+export default useStyles;
