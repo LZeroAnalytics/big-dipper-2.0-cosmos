16ef177be9f2fe2fdc008cb141352d33a4229893
Oleksandr Khlopiachyi <akhlopiachyi@gmail.com>
2025-05-26 14:21:11 +0300

    fix: update usage of logo for light theme in loading screen

--- packages/ui/src/screens/initial_load/index.tsx
+++ packages/ui/src/screens/initial_load/index.tsx
@@ -1,16 +1,19 @@
 import LinearProgress from '@mui/material/LinearProgress';
 import useStyles from '@/screens/initial_load/styles';
 import LogoFull from '@/assets/logo-full.svg';
+import LogoFullLight from '@/assets/logo-full-light.svg';
+import { useSettingList } from '@/components/nav/components/desktop/components/action_bar/components/settings_list/hooks';
 
 const InitialLoad = () => {
   const { classes } = useStyles();
+  const { theme } = useSettingList();
+
+  const isDarkTheme = theme === 'dark';
 
   return (
     <div className={classes.root}>
       <div>
-        <div className={classes.logo}>
-          <LogoFull />
-        </div>
+        <div className={classes.logo}>{isDarkTheme ? <LogoFull /> : <LogoFullLight />}</div>
         <LinearProgress className={classes.divider} />
       </div>
     </div>
