23730e2b0a4b14f145fd9da1e9cb9b75dd4758ec
Oleksandr Khlopiachyi <akhlopiachyi@gmail.com>
2025-05-26 16:45:04 +0300

    fix: update initial_load with memoization of theme value

--- packages/ui/src/screens/initial_load/index.tsx
+++ packages/ui/src/screens/initial_load/index.tsx
@@ -3,17 +3,18 @@ import useStyles from '@/screens/initial_load/styles';
 import LogoFull from '@/assets/logo-full.svg';
 import LogoFullLight from '@/assets/logo-full-light.svg';
 import { useSettingList } from '@/components/nav/components/desktop/components/action_bar/components/settings_list/hooks';
+import { useMemo } from 'react';
 
 const InitialLoad = () => {
   const { classes } = useStyles();
   const { theme } = useSettingList();
 
-  const isDarkTheme = theme === 'dark';
+  const isLightTheme = useMemo(() => theme !== 'dark', [theme]);
 
   return (
     <div className={classes.root}>
       <div>
-        <div className={classes.logo}>{isDarkTheme ? <LogoFull /> : <LogoFullLight />}</div>
+        <div className={classes.logo}>{isLightTheme ? <LogoFullLight /> : <LogoFull />}</div>
         <LinearProgress className={classes.divider} />
       </div>
     </div>
